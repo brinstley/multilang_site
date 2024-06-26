957c51d183c3f8431b35ed276cb5ede78affcb26
Brinstley <brinstley@gmail.com>
2024-06-26 15:19:09 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', '1234')" | python manage.py shell

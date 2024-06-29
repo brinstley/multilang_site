f9ccfea1b2731bf1fc74ba63aec9114e1fe5c878
Brinstley <brinstley@gmail.com>
2024-06-29 23:02:52 +0200

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from .views import home, detail, chatbot
 
+# Definition de nos Urls 
 urlpatterns = [
     path('', home, name= 'home'),
     path('chatbot/', chatbot, name='chatbot'),

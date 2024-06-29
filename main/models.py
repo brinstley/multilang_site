b340b5aab8675271d3bcae180a969e2e2d405343
Brinstley <brinstley@gmail.com>
2024-06-29 23:01:51 +0200

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Creation de notre modele avec les champs title, content et publication date
 class Articles(models.Model):
     title = models.CharField(max_length=50, blank= False, primary_key= True)
     content = models.CharField(max_length=10000, blank= False, default="Description")
     publication_date = models.DateField(auto_now= True)
 
     def __str__(self):
-        return self.title
+        return self.title # Afficher le titre pour chaque article
         
     class Meta:
-        ordering = ['title']
-        verbose_name_plural ="Articles"
+        ordering = ['title'] # Classement se base sur le titre
+        verbose_name_plural ="Articles" # Definition du pluriel
